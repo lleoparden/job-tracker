f78d7ff609d1bfc83a95cf465210e6893a18fe3d
Mostafa Eid <150278458+lleoparden@users.noreply.github.com>
2025-09-11 13:16:54 +0300

    added CORS to be able to connect from frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from app.routers import jobs, scraper
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # restrict later to your frontend domains
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(jobs.router, prefix="/jobs", tags=["Jobs"])
 app.include_router(scraper.router, prefix="/scraper", tags=["Scraper"])
 
-@app.get("/")
-async def root():
-    return {"message": "Job Tracker API running 🚀"}
